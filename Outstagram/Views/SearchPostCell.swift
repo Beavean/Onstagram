068903a8ad28c8679f1cb5f3fb421334b2fb1315
Anthony Petrov <beavean@gmail.com>
2023-02-06 12:10:03 +0200

    feat: add SearchPostCell with configuration

---
+++ Outstagram/Views/SearchPostCell.swift
@@ -0,0 +1,44 @@
+//
+//  SearchPostCell.swift
+//  Outstagram
+//
+//  Created by Beavean on 03.02.2023.
+//
+
+import UIKit
+
+final class SearchPostCell: UICollectionViewCell {
+
+    // MARK: - UI Elements
+
+    var post: Post? {
+        didSet {
+            guard let imageUrl = post?.imageUrl else { return }
+            postImageView.loadImage(with: imageUrl)
+        }
+    }
+
+    private let postImageView: CustomImageView = {
+        let imageView = CustomImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .lightGray
+        return imageView
+    }()
+
+    // MARK: - Properties
+
+    static let reuseIdentifier = String(describing: SearchPostCell.self)
+
+    // MARK: - Lifecycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(postImageView)
+        postImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
