1420b7f269446e8e6f37c988e170791127b6c8b0
Anthony Petrov <beavean@gmail.com>
2023-02-10 11:45:26 +0200

    feat: add ChatCell with configuration

---
+++ Outstagram/Views/ChatCell.swift
@@ -0,0 +1,153 @@
+//
+//  ChatCell.swift
+//  Outstagram
+//
+//  Created by Beavean on 07.02.2023.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import AVFoundation
+
+protocol ChatCellDelegate: AnyObject {
+    func handlePlayVideo(for cell: ChatCell)
+}
+
+final class ChatCell: UICollectionViewCell {
+
+    // MARK: - UI Elements
+
+    let activityIndicatorView: UIActivityIndicatorView = {
+        let activityIndicator = UIActivityIndicatorView(style: .large)
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        activityIndicator.hidesWhenStopped = true
+        return activityIndicator
+    }()
+
+    lazy var playButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setImage(#imageLiteral(resourceName: "play"), for: .normal)
+        button.tintColor = .white
+        button.addTarget(self, action: #selector(handlePlayVideo), for: .touchUpInside)
+        return button
+    }()
+
+    let bubbleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBlue
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 16
+        view.layer.masksToBounds = true
+        return view
+    }()
+
+    let textView: UITextView = {
+        let textview = UITextView()
+        textview.font = UIFont.systemFont(ofSize: 16)
+        textview.backgroundColor = .clear
+        textview.textColor = .white
+        textview.translatesAutoresizingMaskIntoConstraints = false
+        textview.isEditable = false
+        return textview
+    }()
+
+    let profileImageView: CustomImageView = {
+        let imageView = CustomImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .lightGray
+        return imageView
+    }()
+
+    let messageImageView: CustomImageView = {
+        let imageView = CustomImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+
+    // MARK: - Properties
+
+    static let reuseIdentifier = String(describing: ChatCell.self)
+    var bubbleWidthAnchor: NSLayoutConstraint?
+    var bubbleViewRightAnchor: NSLayoutConstraint?
+    var bubbleViewLeftAnchor: NSLayoutConstraint?
+    weak var delegate: ChatCellDelegate?
+    var playerLayer: AVPlayerLayer?
+    var player: AVPlayer?
+    var message: Message? {
+        didSet {
+            if let messageText = message?.messageText {
+                textView.text = messageText
+            }
+            guard let chatPartnerId = message?.getChatPartnerId() else { return }
+            Database.fetchUser(with: chatPartnerId) { user in
+                guard let profileImageUrl = user.profileImageUrl else { return }
+                self.profileImageView.loadImage(with: profileImageUrl)
+            }
+        }
+    }
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+
+        addSubview(bubbleView)
+        bubbleViewRightAnchor = bubbleView.rightAnchor.constraint(equalTo: rightAnchor, constant: -8)
+        bubbleViewRightAnchor?.isActive = true
+        bubbleViewLeftAnchor = bubbleView.leftAnchor.constraint(equalTo: profileImageView.rightAnchor, constant: 8)
+        bubbleViewLeftAnchor?.isActive = false
+        bubbleView.topAnchor.constraint(equalTo: topAnchor, constant: 8).isActive = true
+        bubbleWidthAnchor = bubbleView.widthAnchor.constraint(equalToConstant: 200)
+        bubbleWidthAnchor?.isActive = true
+        bubbleView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+
+        bubbleView.addSubview(messageImageView)
+        messageImageView.anchor(top: bubbleView.topAnchor,
+                                left: bubbleView.leftAnchor,
+                                bottom: bubbleView.bottomAnchor,
+                                right: bubbleView.rightAnchor)
+
+        bubbleView.addSubview(playButton)
+        playButton.centerXAnchor.constraint(equalTo: bubbleView.centerXAnchor).isActive = true
+        playButton.centerYAnchor.constraint(equalTo: bubbleView.centerYAnchor).isActive = true
+        playButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        playButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        playButton.translatesAutoresizingMaskIntoConstraints = false
+
+        bubbleView.addSubview(activityIndicatorView)
+        activityIndicatorView.centerXAnchor.constraint(equalTo: bubbleView.centerXAnchor).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: bubbleView.centerYAnchor).isActive = true
+        activityIndicatorView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        activityIndicatorView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+
+        addSubview(profileImageView)
+        profileImageView.anchor(left: leftAnchor, bottom: bottomAnchor, paddingLeft: 8, paddingBottom: -4, width: 32, height: 32)
+        profileImageView.layer.cornerRadius = 32 / 2
+
+        addSubview(textView)
+        textView.leftAnchor.constraint(equalTo: bubbleView.leftAnchor, constant: 8).isActive = true
+        textView.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+        textView.rightAnchor.constraint(equalTo: bubbleView.rightAnchor).isActive = true
+        textView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        player?.pause()
+        activityIndicatorView.stopAnimating()
+        playerLayer?.removeFromSuperlayer()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Handlers
+
+    @objc func handlePlayVideo() {
+        delegate?.handlePlayVideo(for: self)
+    }
+}
