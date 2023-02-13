2a171df1af1da15954a287c3997276b2693f8db2
Anthony Petrov <beavean@gmail.com>
2023-02-13 14:45:59 +0200

    feat: add HashtagCell with configuration

---
+++ Outstagram/Views/HashtagCell.swift
@@ -0,0 +1,45 @@
+//
+//  HashtagCell.swift
+//  Outstagram
+//
+//  Created by Beavean on 13.02.2023.
+//
+
+import UIKit
+
+final class HashtagCell: UICollectionViewCell {
+
+    // MARK: - UI Elements
+
+    var post: Post? {
+        didSet {
+            guard let imageUrl = post?.imageUrl else { return }
+            postImageView.loadImage(with: imageUrl)
+        }
+    }
+
+    let postImageView: CustomImageView = {
+        let imageView = CustomImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .lightGray
+        return imageView
+    }()
+
+    // MARK: - Properties
+
+    static let reuseIdentifier = "HashtagCell"
+
+
+    // MARK: - Init
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview(postImageView)
+        postImageView.anchor(top: topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
